0ec5a22fece88c87b1fb53b4a60f977b515e3996
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-03-03 19:03:05 +0100

    FIX | add update `Product` route

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,13 +2,13 @@ import express from "express";
 import Product from "../models/Product.js";
 import Category from "../models/Category.js";
 import uploadFile from "../helpers/uploadFile.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
 router.get(`/`, (req, res) => {
   let filter = {};
   if (req.query.categories) {
-    console.log(req.query.categories);
     filter = { category: req.query.categories.split(",") };
   }
 
@@ -67,6 +67,48 @@ router.post(`/`, uploadFile().single("image"), (req, res) => {
     .catch(() => {
       return res.status(400).send("Invalid Category");
     });
+});
+
+router.put("/:id", uploadFile().single("image"), (req, res) => {
+  return Category.findById(req.body.category)
+    .then((category) => {
+      if (!category) {
+        return res.status(400).json({ message: "Invalid Category" });
+      }
+      return Product.findById(req.params.id)
+        .then((product) => {
+          if (!product) {
+            return res.status(400).json({ message: "Invalid Product!" });
+          }
+
+          const file = req.file ? req.file.filename : product.image;
+
+          return product
+            .update({
+              name: req.body.name,
+              description: req.body.description,
+              richDescription: req.body.richDescription,
+              image: file,
+              brand: req.body.brand,
+              price: req.body.price,
+              category: req.body.category,
+              countInStock: req.body.countInStock,
+              rating: req.body.rating,
+              numReviews: req.body.numReviews,
+              isFeatured: req.body.isFeatured,
+            })
+            .then(() =>
+              res.status(200).json({ message: "Product updated successfully!" })
+            )
+            .catch(() =>
+              res
+                .status(500)
+                .json({ message: "the product cannot be updated!" })
+            );
+        })
+        .catch(() => res.status(400).json({ message: "Invalid Product!" }));
+    })
+    .catch(() => res.status(400).json({ message: "Invalid Category" }));
 });
 
 export default router;
