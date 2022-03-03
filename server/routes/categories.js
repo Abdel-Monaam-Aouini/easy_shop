bef1c94687ee216dd2abf94fe6fd2c92def94c95
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-03-03 13:04:51 +0100

    FIX | create new Category

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,28 +1,37 @@
 import express from "express";
-import Category from "../models/category.js";
+import Category from "../models/Category.js";
 
 const router = express.Router();
 
 // get categories List
 router.get(`/`, (req, res) => {
-  return Category.find().then((categoryList) => {
-    if (!categoryList) {
+  return Category.find()
+    .then((categoryList) => res.status(200).json(categoryList))
+    .catch(() => {
       return res.status(500).json({ success: false });
-    }
-    return res.status(200).send(categoryList);
-  });
+    });
 });
 
 // find Category with ID
 router.get("/:id", (req, res) => {
-  return Category.findById(req.params.id).then((category) => {
-    if (!category) {
-      res
+  return Category.findById(req.params.id)
+    .then((category) => res.status(200).send(category))
+    .catch(() => {
+      return res
         .status(500)
         .json({ message: "The category with the given ID was not found." });
-    }
-    res.status(200).send(category);
-  });
+    });
+});
+
+// create a new category
+router.post("/", async (req, res) => {
+  const { name, icon, color } = req.body;
+  return Category.create({ name, icon, color })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => {
+      console.log(`[ERROR] - error creating category: ${err.message}`);
+      return res.status(400).send("the category cannot be created!");
+    });
 });
 
 export default router;
