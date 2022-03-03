76c0008123f0fad86bdc377de1750b6d3ac3b54c
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-03-03 12:06:49 +0100

    FIX | find Category with ID

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -3,12 +3,25 @@ import Category from "../models/category.js";
 
 const router = express.Router();
 
+// get categories List
 router.get(`/`, (req, res) => {
   return Category.find().then((categoryList) => {
     if (!categoryList) {
       return res.status(500).json({ success: false });
     }
     return res.status(200).send(categoryList);
+  });
+});
+
+// find Category with ID
+router.get("/:id", (req, res) => {
+  return Category.findById(req.params.id).then((category) => {
+    if (!category) {
+      res
+        .status(500)
+        .json({ message: "The category with the given ID was not found." });
+    }
+    res.status(200).send(category);
   });
 });
 
