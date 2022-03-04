de2cd57a1e367e2b0b2259c173f88ae66b2593df
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-03-04 15:26:55 +0100

    FIX | add get Order by id `route`

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -3,15 +3,38 @@ import Order from "../models/Order.js";
 
 const ordersRoutes = express.Router();
 
+// GET Orders List
 ordersRoutes.get(`/`, (req, res) => {
   return Order.find()
     .populate("user", "name")
     .sort({ dateOrdered: -1 })
     .then(() => {
       if (!orderList) {
-        res.status(500).json({ success: false });
+        return res.status(500).json({ success: false });
       }
-      res.status(200).json({ success: true, data: orderList });
+      return res.status(200).json({ success: true, data: orderList });
+    })
+    .catch((err) =>
+      res.status(500).json({ success: false, message: err.message })
+    );
+});
+
+// get Order By id
+ordersRoutes.get(`/:id`, (req, res) => {
+  return Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: {
+        path: "product",
+        populate: "category",
+      },
+    })
+    .then((order) => {
+      if (!order) {
+        return res.status(500).json({ success: false });
+      }
+      return res.status(200).json({ success: true, data: order });
     })
     .catch((err) =>
       res.status(500).json({ success: false, message: err.message })
